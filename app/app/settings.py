4d609fefb8022345305bbd7f027482be355d4d08
Fahad Mashi <Fahadmashi@hotmail.com>
2023-12-18 03:59:27 +0300

    New Workflow

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,12 +88,28 @@ DB_HOST = config('POSTGRES_HOST', default='')
 DB_PORT = config('POSTGRES_PORT', default='')
 POSTGRES_READY = config('POSTGRES_READY', default='', cast=bool)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT')
+        }
+    }
 
 CACHES = {
     'default': {
