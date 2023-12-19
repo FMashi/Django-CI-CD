f641a21e33eccf1a5bbe4a745eb0d7674885590e
Fahad Mashi <Fahadmashi@hotmail.com>
2023-12-19 04:36:24 +0300

    U -

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-python app/manage.py migrate
+# entrypoint.sh
+APP_PORT="${PORT:-8000}"
+
+echo "Current working directory: $(pwd)"
+echo "=== Running Django Migrations ==="
+
+source venv/bin/activate || { echo "Failed to activate virtual environment."; exit 1; }
+
+# Navigate to the app directory
+cd /app/ || { echo "Failed to navigate to the 'app' directory."; exit 1; }
+
+# Run migrations
+echo "Applying migrations..."
+python app/manage.py makemigrations || { echo "Failed to create migrations."; exit 1; }
+python app/manage.py migrate || { echo "Failed to apply migrations."; exit 1; }
+python app/manage.py collectstatic --noinput || { echo "collectstatic Failed."; exit 1; }
+
+# Run the development server
+echo "Starting Django development server..."
 python app/manage.py runserver 0.0.0.0:8000
+# /venv/bin/gunicorn --worker-tmp-dir /dev/shm app.wsgi:application --bind "0.0.0.0:${APP_PORT}"
+
